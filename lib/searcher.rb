f1807ee7cb94133d7590593ddc0f5eccd80952dc
Ryan Bigg <radarlistener@gmail.com>
2010-10-04 11:25:24 +1100

    Move code into after_initialize block so that we can define scopes rather than doing it on the fly.

--- lib/searcher.rb
+++ lib/searcher.rb
@@ -2,7 +2,32 @@ require 'active_record'
 require 'searcher/class_methods'
 
 module Searcher
-  # Your code goes here...
+  mattr_accessor :classes
+  def self.classes
+    @classes ||= []
+  end
 end
 
 ActiveRecord::Base.extend(Searcher::ClassMethods)
+
+ActiveSupport.on_load(:after_initialize) do
+  p Searcher.classes
+  # @externals].each do |name, config|
+  #     association = reflect_on_association(config[:from])
+  #     association_table = Table(association.klass.table_name)
+  #     case association.macro
+  #       when :has_and_belongs_to_many
+  #         join_table = Table(association.options[:join_table])
+  #   
+  #         jtl = table[klass.primary_key]
+  #         jtr = join_table[association.primary_key_name]
+  #   
+  #         other_primary_key = association_table[association.klass.primary_key]
+  #         association_foreign_key = join_table[association.association_foreign_key]
+  #   
+  #         self.send(:scope, "by_#{config[:field]}", lambda { |q| join(join_table).on(jtl.eq(jtr)).
+  #         join(association_table).on(other_primary_key.eq(association_foreign_key)).
+  #         where(association_table[external[:field]].eq(q)) })
+  #     end
+  #   end
+end
--- lib/searcher/class_methods.rb
+++ lib/searcher/class_methods.rb
@@ -3,6 +3,7 @@ require 'searcher/config'
 module Searcher
   module ClassMethods
     def searcher(&block)
+      Searcher.classes << self
       @config ||= Searcher::Config.new.instance_exec(&block)
     end
     
@@ -15,35 +16,19 @@ module Searcher
           external, q = piece.split(":")
           external = @config[:externals][external.to_sym]
           next unless external
-          
-          # Find the type of association we're dealing with
-          association = reflect_on_association(external[:from])
-          association_table = Table(association.klass.table_name)
-          case association.macro
-            
-            when :has_and_belongs_to_many
-              join_table = Table(association.options[:join_table])
+          send("by_#{external[:field]}", q)
               
-              jtl = table[klass.primary_key]
-              jtr = join_table[association.primary_key_name]
               
-              other_primary_key = association_table[association.klass.primary_key]
-              association_foreign_key = join_table[association.association_foreign_key]
-              
-              klass.find_by_sql(table.join(join_table).on(jtl.eq(jtr)).
-              join(association_table).on(other_primary_key.eq(association_foreign_key)).
-              where(association_table[external[:field]].eq(q)).to_sql)
-              
-            when :belongs_to
-              foreign_key = table[association.primary_key_name]
-              other_primary_key = association_table[association.klass.primary_key]
-              
-              sql = table.join(association_table).on(foreign_key.eq(other_primary_key)).
-              where(association_table[external[:field]].eq(q)).to_sql
-              
-              klass.find_by_sql(table.join(association_table).on(foreign_key.eq(other_primary_key)).
-              where(association_table[external[:field]].eq(q)).to_sql) 
-          end
+          #   when :belongs_to
+          #     foreign_key = table[association.primary_key_name]
+          #     other_primary_key = association_table[association.klass.primary_key]
+          #     
+          #     sql = table.join(association_table).on(foreign_key.eq(other_primary_key)).
+          #     where(association_table[external[:field]].eq(q)).to_sql
+          #     
+          #     klass.find_by_sql(table.join(association_table).on(foreign_key.eq(other_primary_key)).
+          #     where(association_table[external[:field]].eq(q)).to_sql) 
+          # end
           
         else
           k.where(table[@config[:default]].matches_any("%#{piece}%"))
